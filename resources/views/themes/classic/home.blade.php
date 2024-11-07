c07c11321d34022c0aaf62b0e9157360f3f58fd0
Владимир Костюченко <webcodingby@yandex.by>
2024-11-07 18:19:57 +0300

    add theme form and components

--- resources/views/themes/classic/home.blade.php
+++ resources/views/themes/classic/home.blade.php
@@ -1,1 +1,12 @@
 <h1>Главная страница - Тема Classic</h1>
+
+<form action="{{ route('set.theme') }}" method="POST">
+    @csrf
+    <label for="theme">Выберите тему:</label>
+    <select name="theme" id="theme">
+        <option value="classic">Classic</option>
+        <option value="cuba">Cuba</option>
+    </select>
+    <button type="submit">Сохранить</button>
+</form>
+
--- resources/views/themes/cuba/home.blade.php
+++ resources/views/themes/cuba/home.blade.php
@@ -1,1 +1,11 @@
 <h1>Главная страница - Тема Cuba</h1>
+
+<form action="{{ route('set.theme') }}" method="POST">
+    @csrf
+    <label for="theme">Выберите тему:</label>
+    <select name="theme" id="theme">
+        <option value="classic">Classic</option>
+        <option value="cuba">Cuba</option>
+    </select>
+    <button type="submit">Сохранить</button>
+</form>
