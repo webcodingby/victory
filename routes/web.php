3a6c166749f43e48d7b1a7c4723f8f910f6c73a2
Владимир Костюченко <webcodingby@yandex.by>
2024-11-07 18:17:24 +0300

    add theme route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ThemeController;
+
+Route::get('/', [ThemeController::class, 'home'])->name('home');
+Route::get('/contact', [ThemeController::class, 'contact'])->name('contact');
+Route::post('/set-theme', [ThemeController::class, 'setTheme'])->name('set.theme');
+
